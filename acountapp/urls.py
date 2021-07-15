b5b519b4b928f5d3d734a24dd11f4e056d67ecbe
ivy_mimi <chlgoals54@likelion.org>
2021-07-15 17:10:26 +0900

    7/15 haemin_web01 class commit 1

--- acountapp/urls.py
+++ acountapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from acountapp.views import hello_world
+from acountapp.views import hello_world,AccountCreateView
+
+app_name = 'acountapp'
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/',AccountCreateView.as_view(),name = 'create')
 ]
--- acountapp/views.py
+++ acountapp/views.py
@@ -1,8 +1,31 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import  HttpResponseRedirect
 from django.shortcuts import render
-
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
-def hello_world(request):
-    return render(request,'acountapp/hello_world.html')
+from django.views.generic import CreateView
 
+from acountapp.models import HelloWorld
+
+
+def hello_world(request):
+    if request.method =="POST":
+
+        temp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('acountapp:hello_world'))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request,'acountapp/hello_world.html',context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('acountapp:hello_world')
+    template_name = "acountapp/create.html"
